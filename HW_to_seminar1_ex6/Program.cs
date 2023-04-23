ec644eb154692a6938370bed9b9e0172fe1ed718
Nata <nata3010@list.ru>
2023-04-24 00:24:34 +0300

    add decision HW_ex6

---
+++ HW_to_seminar1_ex6/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 6: 
+// Напишите программу,
+// которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+/*
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+/*
+Алгоритм:
+1. Запросить число
+2. Спросить: "Четное или нечетное введенное число?"
+3. Проверить на чётность
+4. Вывести сообщение
+*/
+
+Console.Write("Input your number = ");
+double number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Is your number EVEN or Odd?");
+
+if (number %2 == 0) 
+{
+Console.WriteLine("Your number is EVEN!");
+}
+else
+{
+Console.WriteLine("Your number is Odd!");
+}
