69ffa3851e787868d4438a00bfb317bf8bfb3d0a
Nata <nata3010@list.ru>
2023-04-29 22:43:56 +0300

    add Exercise10

---
+++ HW_to_seminar2_ex10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10:
+//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5, 782 -> 8, 918 -> 1
+
+void Vew2digit(int number)
+{
+int a = number % 100;
+int a2 = a / 10;
+Console.WriteLine(a2);
+}
+
+Console.WriteLine("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+Vew2digit(number);
+else Console.WriteLine("Your number must be three digits!");
