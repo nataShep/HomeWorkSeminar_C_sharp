a49c6c98c0812f267bca966a8c5b309963cb3ecd
Nata <nata3010@list.ru>
2023-05-05 09:24:40 +0300

    add Exercise13

---
+++ HW_to_seminar2_ex13/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 13: 
+// Напишите программу, 
+// которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+
+void View3num(int num)
+{
+    int a = num / 10;
+        while (a>1000 || a<-1000) a=a/10;
+            if (a > 99 || a < -99) 
+        {
+          a=a%10;
+          if (a > 0)
+            {
+            int num3 = a;
+            Console.WriteLine("3rd numeral of your number: " + num3);
+            }
+            else
+            {
+               int num3 = - a;
+               Console.WriteLine("3rd numeral of your number: " + num3); 
+            }
+        }
+
+        else
+        {
+            if (a > 0)
+            {
+            int num3 = num - (a*10);
+            Console.WriteLine("3rd numeral of your number: " + num3);
+            }
+            else
+            {
+               int num3 = - (num - (a*10));
+               Console.WriteLine("3rd numeral of your number: " + num3); 
+            }
+        }
+}
+
+Console.WriteLine("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 100 && number > -100)
+        Console.WriteLine("Your number hasn't 3rd numeral");
+    else View3num(number);
