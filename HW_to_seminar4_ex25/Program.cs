9066dfe72f70c35a9ad3f77807aaca8fe1fc6e5e
Nata <nata3010@list.ru>
2023-05-22 00:12:13 +0300

    add Exercise25

---
+++ HW_to_seminar4_ex25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵),
+// 2, 4 -> 16
+
+int Power ()
+{
+Console.Write("Input number1 = ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number2 = ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int P = 1;
+for (int a = 1; a <= number2; a++)
+{
+    P = P * number1;
+}
+return P;
+}
+
+int res = Power ();
+Console.WriteLine($"Result number1^number2 = {res}");
