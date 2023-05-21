dd6aca2c348e01bf3e0bae57a8856dfd53d8547a
Nata <nata3010@list.ru>
2023-05-21 20:06:45 +0300

    add Exercise23

---
+++ HW_to_seminar3_ex23/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Cube()
+{
+    Console.Write("Input number = ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num>0)
+    {
+        int currentN = 1;
+        while (currentN <= num)
+        {
+            int res = currentN*currentN*currentN;
+            Console.Write($"{res}, ");
+            currentN++;
+        }
+        Console.WriteLine("\b\b.");
+    }
+    //else Console.WriteLine("Impossible value!");
+    else
+    {
+        int currentN = 1;
+        while (currentN >= num)
+        {
+            int res = currentN*currentN*currentN;
+            Console.Write($"{res}, ");
+            currentN--;
+        }
+        Console.WriteLine("\b\b.");
+    }
+}
+Cube();
