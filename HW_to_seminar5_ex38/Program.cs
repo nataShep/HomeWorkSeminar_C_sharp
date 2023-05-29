1f47b8ad9ce55d316228d28e6a8867ed1d358752
Nata <nata3010@list.ru>
2023-05-29 08:56:14 +0300

    add Exercise38

--- HW_to_seminar5_ex38/Program.cs
+++ HW_to_seminar5_ex38/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double [] ArrayRandom (int size, int min, int max)
+{
+    double [] array38 = new double [size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array38[i] = rand.Next(min, max+1) + rand.NextDouble();
+    }    
+    return array38;
+}
+
+void ShowArray (double [] arrayN)
+{
+    for (int i = 0; i < arrayN.Length; i++)
+       Console.WriteLine (arrayN[i] + " ");  
+}
+
+double FindMinNum (double [] minNum)
+{
+    double minVal = minNum [0];
+    for (int i = 0; i < minNum.Length; i++)
+        if (minNum[i] < minVal)
+        minVal = minNum[i];
+    return minVal;
+}
+
+double FindMaxNum (double [] maxNum)
+{
+    double maxVal = maxNum [0];
+    for (int i = 0; i < maxNum.Length; i++)
+        if (maxNum[i] > maxVal)
+        maxVal = maxNum[i];
+    return maxVal;
+}
+
+Console.Write ("Input count of elements in array = ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input first number = ");
+int minN = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input second number = ");
+int maxN = Convert.ToInt32(Console.ReadLine());
+    if (minN < maxN)
+    {
+        double [] Array = ArrayRandom (sizeArray, minN, maxN);
+        ShowArray (Array);
+        double a = FindMinNum (Array); 
+        double c = FindMaxNum (Array);
+        Console.WriteLine($"The difference between the maximum and minimum number of an array = {c - a}");
+    }
+    else
+        Console.WriteLine("First number should be less then Second number!");
