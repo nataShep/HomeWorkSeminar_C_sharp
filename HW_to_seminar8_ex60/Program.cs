279aaf75307c3a782ce4c91ced1fb5044527916c
Nata <nata3010@list.ru>
2023-06-10 03:33:36 +0300

    add Exercise60

---
+++ HW_to_seminar8_ex60/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] array3D = new int[2, 2, 2];
+
+void Array(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int a = 0; a < arr.GetLength(2); a++)
+            {
+                arr[i, j, a] = new Random().Next()%345;
+            }
+        }
+    }
+}
+
+void PrintPosition(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int a = 0; a < array3D.GetLength(2); a++)
+            {
+                Console.Write($"{array3D[i, j, a]%100}({i},{j},{a}) ");
+            }
+        }
+    }
+}
+
+Array(array3D);
+PrintPosition(array3D);
